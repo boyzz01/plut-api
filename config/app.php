041d985bdfb9033ab32744e8bcd1363c643d3ad0
Ardi Sahputra <ardysyahputra01@gmail.com>
2022-04-03 13:57:15 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://ardisahputra.me/plut/'),
+    'url' => env('APP_URL', 'https://ardisahputra.me'),
 
     'asset_url' => env('ASSET_URL', null),
 
