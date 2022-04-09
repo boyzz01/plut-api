b6376f6750733838918f2d6465196adafede4105
Ardi Sahputra <ardysyahputra01@gmail.com>
2022-04-09 16:48:05 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,5 @@ Route::get('kategori',[ApiController::class, 'get_kategori']);
 Route::get('umkm',[ApiController::class, 'get_umkm']);
 Route::get('produk',[ApiController::class, 'get_produk']);
 Route::any('/login', [ApiController::class,'check_user'] );
-Route::post('add_umkm', [ApiController::class,'addUmkm'] )
-->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::post('/add_umkm', [ApiController::class,'addUmkm'] );
 Route::any('/add_produk', [ApiController::class,'add_produk'] );
