e459a5022bfd3bf24e760e54532569f603b0e152
Ardi Sahputra <ardysyahputra01@gmail.com>
2022-04-17 01:43:20 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -348,7 +348,7 @@ class ApiController extends Controller
 
     public function get_transaksi($id){
         $transaksi = DB::select("SELECT transaksi.*,user.username FROM `transaksi` JOIN user ON transaksi.id_user = user.id WHERE transaksi.id_transaksi = '$id' AND transaksi.deleted = '0'");
-        $item = DB::select("SELECT transaksi_item.*,barang.nama,barang.harga FROM `transaksi_item` JOIN barang ON transaksi_item.id_product = barang.kode_produk WHERE transaksi_item.id_transaksi = '$id'");
+        $item = DB::select("SELECT transaksi_item.*,barang.nama,barang.harga FROM `transaksi_item` JOIN barang ON transaksi_item.id_product = barang.kode_produk WHERE transaksi_item.id_transaksi = '$id' AND transaksi.deleted = '0' ");
        // $item =DB::table("transaksi_item")->where('id_transaksi','=',$id)->where('deleted','=','0')->get();
 
         return response()
