99dfa1713353f0f8b2469919918cf3771edce186
Ardi Sahputra <ardysyahputra01@gmail.com>
2022-04-17 01:20:38 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -329,8 +329,7 @@ class ApiController extends Controller
 
             DB::table("keranjang")->where('user_id','=',$request->user_id)->delete();
 
-            return response()
-            ->json([
+            return response()->json([
                 'success' => true,
                 'id_trans'=>$idtrans
             ]);
@@ -338,13 +337,16 @@ class ApiController extends Controller
     }
 
     public function get_transaksi($id){
-        $transaksi = DB::select("SELECT transaksi.*,user.username FROM `transaksi` JOIN user ON transaksi.id_user = user.id WHERE transaksi.id_transaksi = '$id' AND transaksi.deleted = '0'");
-        $item =DB::table("transaksi_item")->where('id_transaksi','=',$id)->where('deleted','=','0')->get();
-
+      
+        DB::transaction(function () use ($id){
+            $transaksi = DB::select("SELECT transaksi.*,user.username FROM `transaksi` JOIN user ON transaksi.id_user = user.id WHERE transaksi.id_transaksi = '$id' AND transaksi.deleted = '0'");
+            $item =DB::table("transaksi_item")->where('id_transaksi','=',$id)->where('deleted','=','0')->get();
+    
         return response()
         ->json([
             'transaksi' => $transaksi[0],
             'item'=>$item
         ]);
+    });
     }
 }
