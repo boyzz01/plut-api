89806b7d218db7bf958e4ce108df26593ed1c776
Ardi Sahputra <ardysyahputra01@gmail.com>
2022-11-14 11:33:28 +0700

    a

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -15,48 +15,48 @@ class ReportController extends Controller
     }
 
     public function slow(){
-        $data= DB::select("SELECT SUM(total_produk) as 'total',transaksi_item.id_product,barang.* FROM transaksi_item JOIN barang ON barang.kode_produk = transaksi_item.id_product GROUP BY transaksi_item.id_product ORDER by total ASC, barang.nama ASC");
+        $data= DB::select("SELECT SUM(total_produk) as 'total',transaksi_item.id_product,barang.*,umkm.nama as 'nama_umkm' FROM transaksi_item JOIN barang ON barang.kode_produk = transaksi_item.id_product JOIN umkm ON barang.kode_umkm = umkm.kode_umkm GROUP BY transaksi_item.id_product ORDER by total ASC, barang.nama ASC");
          return view('report_fast',['data'=>$data]);
         
     }
 
     public function umkm(){
-        $data= DB::select("SELECT SUM(transaksi_item.total_produk) as 'total',transaksi_item.id_product,umkm.* FROM transaksi_item JOIN barang ON barang.kode_produk = transaksi_item.id_product JOIN umkm ON barang.kode_umkm = umkm.kode_umkm GROUP BY umkm.kode_umkm ORDER by total DESC, barang.nama ASC");
-         return view('report_fast',['data'=>$data]);
+        $data= DB::select("SELECT SUM(transaksi_item.total_produk) as 'total_p',transaksi_item.id_product,umkm.* FROM transaksi_item JOIN barang ON barang.kode_produk = transaksi_item.id_product JOIN umkm ON barang.kode_umkm = umkm.kode_umkm GROUP BY umkm.kode_umkm ORDER by total_p DESC, barang.nama ASC");
+         return view('report_umkm',['data'=>$data]);
     }
 
     public function kota(){
         $data= DB::select("SELECT SUM(transaksi_item.total_produk) as 'total',transaksi_item.id_product,ms_kota.* FROM transaksi_item JOIN barang ON barang.kode_produk = transaksi_item.id_product  JOIN ms_kota ON ms_kota.kode = barang.kode_kota GROUP BY barang.kode_kota ORDER by total DESC, barang.nama ASC");
-         return view('report_fast',['data'=>$data]);
+         return view('report_kota',['data'=>$data]);
     }
 
     public function kategori(){
         $data= DB::select("SELECT SUM(transaksi_item.total_produk) as 'total',transaksi_item.id_product,ms_kategori.* FROM transaksi_item JOIN barang ON barang.kode_produk = transaksi_item.id_product  JOIN ms_kategori ON ms_kategori.kode = barang.kode_kategori GROUP BY barang.kode_kota ORDER by total DESC, barang.nama ASC");
-         return view('report_fast',['data'=>$data]);
+         return view('report_kota',['data'=>$data]);
     }
 
     public function stock_all(){
-        $data = DB::select("SELECT * FROM `barang` ORDER BY updated_at ASC");
-         return view('report_fast',['data'=>$data]);
+        $data = DB::select("SELECT barang.*,umkm.nama as 'nama_umkm' FROM barang  JOIN umkm ON barang.kode_umkm = umkm.kode_umkm ORDER BY updated_at ASC");
+         return view('stock',['data'=>$data]);
     }
 
     public function stock_umkm(){
-        $data = DB::select("SELECT SUM(barang.stock) AS 'total',umkm.* FROM barang JOIN umkm ON barang.kode_umkm = umkm.kode_umkm GROUP BY umkm.kode_umkm ORDER by total DESC, barang.nama ASC");
-         return view('report_fast',['data'=>$data]);
+        $data = DB::select("SELECT SUM(barang.stock) AS 'total_p',umkm.* FROM barang JOIN umkm ON barang.kode_umkm = umkm.kode_umkm GROUP BY umkm.kode_umkm ORDER by total_p DESC");
+         return view('stock_umkm',['data'=>$data]);
     }
 
     public function stock_kategori(){
         $data = DB::select("SELECT SUM(barang.stock) AS 'total',ms_kategori.* FROM barang JOIN ms_kategori ON barang.kode_kategori = ms_kategori.kode GROUP BY ms_kategori.kode ORDER by total DESC, barang.nama ASC");
-         return view('report_fast',['data'=>$data]);
+         return view('stock_kota',['data'=>$data]);
     }
 
     public function stock_outdate(){
-        $data = DB::select("SELECT * FROM `barang` ORDER BY updated_at DESC");
-         return view('report_fast',['data'=>$data]);
+        $data = DB::select("SELECT barang.*,umkm.nama as 'nama_umkm' FROM barang  JOIN umkm ON barang.kode_umkm = umkm.kode_umkm ORDER BY updated_at DESC");
+         return view('stock',['data'=>$data]);
     }
 
     public function stock_zero(){
-        $data = DB::select("SELECT * FROM barang WHERE stock = 0");
-         return view('report_fast',['data'=>$data]);
+        $data = DB::select("SELECT barang.*,umkm.nama as 'nama_umkm' FROM barang  JOIN umkm ON barang.kode_umkm = umkm.kode_umkm WHERE stock = 0");
+         return view('stock',['data'=>$data]);
     }
 }
---
+++ resources/views/stock_umkm.blade.php
@@ -0,0 +1,73 @@
+@extends('main2')
+
+@section('container')
+
+    <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+       
+        <div class="container-fluid">
+
+
+
+    
+            <div class="card card-custom">
+             
+                <div class="card-body">
+                    <!--begin: Datatable-->
+                    <table class="table table-bordered table-checkable" id="kt_datatable2">
+                        <thead>
+                            <tr>
+                                <th style="width: 5%">No</th>
+                              
+                                <th>UMKM</th>
+                                <th>Stock</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+
+
+                            @php
+                                $nomor = 1;
+                            @endphp
+                            @foreach ($data as $d)
+                                <tr id="tr{{ $d->id }}">
+                                    <td style=" width:5%">{{ $nomor }}</td>
+                                   
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <div class="symbol symbol-circle symbol-35 mr-3">
+                                                <p> <a href="#" class="ratio img-responsive img-circle" style="background-image: url({{ $d->foto }});width:50px"></a>
+                                                </p>  {{-- <img class="rounded-circle" alt="Pic" src="{{ $d->foto }}"> --}}
+                                            </div>
+                                            <div>
+                                                <a href="#" class="text-dark-75 "> &nbsp{{ $d->nama }}</a>
+                                            
+                                            </div>
+                                        </div>
+                                    </div></td>
+                                   
+                                    <td>{{ $d->total_p }}</td>
+                                  
+                                </tr>
+
+                                @php
+                                    $nomor++;
+                                @endphp
+                            @endforeach
+
+
+
+
+                        </tbody>
+                    </table>
+                    <!--end: Datatable-->
+                </div>
+            </div>
+
+        </div>
+        <!-- Container-fluid starts-->
+
+    </div>
+    <!-- Container-fluid Ends-->
+    </div>
+@endsection
